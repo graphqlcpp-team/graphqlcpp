6d4f3022f6fb1452aacb5656adf07e388e00557d
amelie <schmidtam67936@th-nuernberg.de>
2019-02-10 22:25:07 +0100

    factory added

---
+++ src/ApiFactory.h
@@ -0,0 +1,26 @@
+/*
+ * ApiFactory.h
+ *
+ *  Created on: 10.02.2019
+ *      Author: amelie
+ */
+
+#ifndef APIFACTORY_H_
+#define APIFACTORY_H_
+
+#include "GraphqlCppApi.h"
+
+namespace graphqlcpp {
+namespace api {
+
+class ApiFactory {
+	private:
+		ApiFactory();
+	public:
+		static GraphqlCppApi* createApi();
+};
+
+} /* namespace api */
+} /* namespace graphqlcpp */
+
+#endif /* APIFACTORY_H_ */
