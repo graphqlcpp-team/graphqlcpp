e5ba429058a0e3c8daa00fc5ddedc7080c195a56
Julian Fruth <j.fruth2@gmail.com>
2019-03-26 20:15:44 +0100

    test fix

--- tests/basic_tests/GraphqlCppApiTest.cpp
+++ tests/basic_tests/GraphqlCppApiTest.cpp
@@ -15,16 +15,14 @@ using namespace graphqlcpp::api;
 
 TEST(GraphQlApiTest, dumpAstAsJson){
 	const char * query = "query{user(id:1) {name}}";
-
 	const char* schema = "schema {query: Query, mutation: Mutation} type Query { user(id: ID!): User} type User { id: ID! name: string!	age: Int}";
-	//const char* simpleSchema = "{ me { name } }";
+
 
 	GraphqlCppApi* api = ApiFactory::createApi();
 	api->setSchema(schema);
-	cout << api->executeQuery(query);
-	//auto json = api->printSchemaAsJson();
-	//cout << json;
-	//ASSERT_NE(nullptr, json);
+	auto result = api->executeQuery(query);
+
+	ASSERT_NE(nullptr, result);
 
 }
 
