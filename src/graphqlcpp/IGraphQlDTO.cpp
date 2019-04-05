898ab6b08f8923a58f1701351dc623a6571bcc07
Julian Fruth <j.fruth2@gmail.com>
2019-04-05 22:07:20 +0200

    Merge branch 'master' of git.informatik.fh-nuernberg.de:itprojectgraphql/graphqlcpp-vmtest into Argument_Validation
    # Conflicts:
    #	include/graphqlcpp/validators/SchemaAstWraper.h

---
+++ src/graphqlcpp/IGraphQlDTO.cpp
@@ -0,0 +1,5 @@
+//
+// Created by julian on 05.04.19.
+//
+
+#include "IGraphQlDTO.h"
---
+++ src/graphqlcpp/Serializer.cpp
@@ -0,0 +1,20 @@
+//
+// Created by julian on 05.04.19.
+//
+
+#include "Serializer.h"
+
+
+void Serializer::setVal(char **name, IGraphQlDTO *val) {
+    if(wasFieldRequested(name)){
+
+    }
+}
+
+bool Serializer::wasFieldRequested(char **fieldName) {
+    return false;
+}
+
+
+
+
