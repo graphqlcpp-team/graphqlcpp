7a2f34943652e12d31d519d69eacdc90b541e796
amelie <schmidtam67936@th-nuernberg.de>
2019-04-18 10:13:43 +0200

    bugfix

--- src/graphqlcpp/RequestAstWrapper.cpp
+++ src/graphqlcpp/RequestAstWrapper.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "../../include/graphqlcpp/RequestAstWrapper.h"
+
+std::string graphqlcpp::RequestAstWrapper::extractOperation() {
+    return std::__cxx11::string();
+}
+
+graphqlcpp::ResolverInfo *graphqlcpp::RequestAstWrapper::extractResolver() {
+    return nullptr;
+}
+
+facebook::graphql::ast::SelectionSet *graphqlcpp::RequestAstWrapper::extractSelectionSetForSerialisation() {
+    return nullptr;
+}
--- src/graphqlcpp/dispatcher/RequestDispatcher.cpp
+++ src/graphqlcpp/dispatcher/RequestDispatcher.cpp
@@ -3,10 +3,17 @@
 //
 
 #include "../../../include/graphqlcpp/dispatcher/RequestDispatcher.h"
+#include "../../../include/graphqlcpp/resolver/ResolverInfo.h"
+#include "../../../include/graphqlcpp/IGraphQlDTO.h"
+#include "../../../include/graphqlcpp/utils/MySerializer.h"
+#include "../../../include/graphqlcpp/utils/MyWriter.h"
+#include "../../../include/graphqlcpp/RequestAstWrapper.h"
+#include <string>
+
 
 
 std::string
-graphqlcpp::dispatcher::RequestDispatcher::executeRequest(RequestAstWrapper *requestAstWrapper) {
+graphqlcpp::dispatcher::RequestDispatcher::executeRequest(graphqlcpp::RequestAstWrapper *requestAstWrapper) {
     std::string operation = requestAstWrapper->extractOperation();
     if (operation == "query") {
         ResolverInfo * resolver;
