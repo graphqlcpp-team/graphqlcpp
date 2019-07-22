8bbd70b35a7d72b5809a1e0fd256cf3632f9a68a
Julian Fruth <j.fruth2@gmail.com>
2019-07-22 23:10:55 +0200

    comments added

--- include/graphqlcpp/GraphqlCppApi.h
+++ include/graphqlcpp/GraphqlCppApi.h
@@ -22,52 +22,104 @@
 #include "RequestAstWrapper.h"
 
 namespace graphqlcpp {
-namespace api {
-using namespace facebook::graphql::ast;
-using namespace validators;
-using namespace graphqlcpp::exceptions;
-using namespace resolver;
-using namespace dispatcher;
+    namespace api {
+        using namespace facebook::graphql::ast;
+        using namespace validators;
+        using namespace graphqlcpp::exceptions;
+        using namespace resolver;
+        using namespace dispatcher;
 
 
-class GraphqlCppApi {
-private:
-	SchemaAstWraper* schemaWraper = nullptr;
-	SchemaValidator* schemaValidator = nullptr;
-	QueryValidator* queryValidator = nullptr;
-	ResolverManager* resolverManager = nullptr;
-	RequestDispatcher* requestDispatcher = nullptr;
-    std::string introspectionSchema = "";
-    /**
-     * The constructor of the GraphqlCpp Class
-     */
-	GraphqlCppApi();
-	bool checkIfRequestValid(Node* rootNodeRequest);
-	Node* parseStringToAst(const std::string &str);
-	Node* parseSchemaStringToSchemaAst(const std::string &str);
+        class GraphqlCppApi {
+        private:
+            SchemaAstWraper *schemaWraper = nullptr;
+            SchemaValidator *schemaValidator = nullptr;
+            QueryValidator *queryValidator = nullptr;
+            ResolverManager *resolverManager = nullptr;
+            RequestDispatcher *requestDispatcher = nullptr;
+            std::string introspectionSchema = "";
 
-public:
-	~GraphqlCppApi();
+            /**
+             * The constructor of the GraphqlCpp Class. The Ctor is private. It should be created via the Factory Method
+             */
+            GraphqlCppApi();
 
-	/**
-	 * This function
-	 * @param schema
-	 */
-	void setSchema(const std::string &schema);
-    void setGraphiQlIntrospectionSchema(std::string introspectionSchema);
-    std::string getGraphiQlIntrospectionSchema();
-	void registerResolver(IGraphQlResolver* resolver);
-    string executeRequest(const std::string &request);
+            /***
+             * This method checks wether a request is valid or not
+             * @param rootNodeRequest The root node of the query ast
+             * @return true if the request is valid otherwise it returns false
+             */
+            bool checkIfRequestValid(Node *rootNodeRequest);
 
-    const char * printSchemaAsJson();
+            /***
+             * This functions converts a request string to an ast using the facebook graphqlparser library
+             * @param str the string to parse
+             * @return the root node of the ast
+             */
+            Node *parseStringToAst(const std::string &str);
 
-    /***
-     * Factory method for GraphQlCppApi
-     * @return
-     */
-    static GraphqlCppApi* createInstance();
-};
+            /***
+             * This functions converts a schema string to ast using the facebook graphqlparser library
+             * @param str the schema string to parse
+             * @return the root node of the schema ast
+             */
+            Node *parseSchemaStringToSchemaAst(const std::string &str);
 
-} /* namespace graphqlcppapi */
+        public:
+            /***
+             * Dstructor
+             */
+            ~GraphqlCppApi();
+
+            /**
+             * This function function is used to register the graphql schema. The schema must match with your DTOs and
+             * the resolvers you register. You need to register a schema before you can use this library. Check out
+             * the IntegrationTests to find some examples
+             * @param schema
+             */
+            void setSchema(const std::string &schema);
+
+            /**
+             * Use this method to register your graphql introspection schema. This is needed for type introspection
+             * with graphiql. If you don't use graphiql you don't need this.
+             * @param introspectionSchema the string with the introspection schema.
+             */
+            void setGraphiQlIntrospectionSchema(std::string introspectionSchema);
+
+            /**
+             * Returns the graphiql introspection schema
+             * @return the introspection schema
+             */
+            std::string getGraphiQlIntrospectionSchema();
+
+            /**
+             * Here you need to register your resolvers before you can use this library. The resolver must match the
+             * registered schema. Checkout the IntegrationTests to find an example.
+             * @param the resolver is called if its requested in the request
+             */
+            void registerResolver(IGraphQlResolver *resolver);
+
+            /**
+             * Call this method to execute a graphql request. Before you need to set a schema an register the matching
+             * resolvers.
+             * @param the graphql request
+             * @return the graphql response
+             */
+            string executeRequest(const std::string &request);
+
+            /**
+             * Only for debugging. Returns the registered schema as Json
+             * @return the registerd schema as json
+             */
+            const char *printSchemaAsJson();
+
+            /***
+             * Factory method for GraphQlCppApi
+             * @return
+             */
+            static GraphqlCppApi *createInstance();
+        };
+
+    } /* namespace graphqlcppapi */
 }
 #endif /* GRAPHQLCPPAPI_H_ */
